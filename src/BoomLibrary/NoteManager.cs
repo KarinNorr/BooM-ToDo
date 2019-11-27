ab5d471bc227331165c654ec9f7a2255f2fb97d6
Karin Norrlander <karin@barprego.se>
2019-11-27 13:52:55 +0100

    developing statusmethods

--- src/BoomLibrary/NoteManager.cs
+++ src/BoomLibrary/NoteManager.cs
@@ -1,42 +1,55 @@
+using System.Collections.Generic;
 namespace BoomLibrary
 {
 
     //Enum styr vilken view som ska visas av metod för att visa Note
-    public enum NoteView{ Basic=1, Light, Expanded }
-    
-    
+    public enum NoteView { Basic = 1, Light, Expanded }
+
     public class NoteManager
     {
         //variabel som håller håller koll på när senaste review gjordes
-        public int ReviewTime =0;
+        public int ReviewTime = 0;
 
         //metod som tar bort en note
-        public void DeleteNote()
+        public string DeleteNote(Note note, List<Note> listofnotes)
         {
+            try
+            {
+                int noteIndex = listofnotes.FindIndex(note);
+            }
+            catch
+            {
+                return "Det fanns ingen sån här note";
 
+            }
+            listofnotes.RemoveAt(noteIndex);
+            return "Nu är din note borta!";
         }
-        
+
         //metod som visar en note 
         //kallar på andra metoder beroende på inparametrar
         //En enum styr vilken vy som kommer att visas 
 
         public string ShowNote(NoteView view)
         {
-            switch(view)
+            switch (view)
             {
                 case 1:
-                return;
+                    GetBasicView();
+                    return;
 
                 case 2:
-                return;
+                    GetLightView();
+                    return;
 
                 case 3:
-                return;
+                    GetExpandedView();
+                    return;
 
                 default:
             }
         }
-        
+
         //Visar endast titel på en Note 
         private string GetBasicView(Note note)
         {
@@ -44,7 +57,7 @@ namespace BoomLibrary
             return "Denna Note heter: " + title;
 
         }
-        
+
         //Light view, titel plus status
         private string GetLightView(Note note)
         {
@@ -61,15 +74,21 @@ namespace BoomLibrary
         }
 
         //Metod som ändrar status på en note
-        public int ChangeStatus(int status)
+        public void ChangeStatus(Note note, GTD_Status status)
         {
-            return status;
+            note.SetStatus(status);
         }
-        
-        //Metod som flyttar Note framåt i flödet
-        public int MoveNoteForward()
-        {
 
+        //Metod som flyttar Note framåt i flödet
+        public int MoveNoteForward(Note note)
+        {
+            currentStatus = note.GetStatus;
+            if(currentStatus = note.GetStatus.
+            note.SetStatus(currentStatus++);
+
+
+
+            //note.SetStatus(note.GetStatus++);
         }
 
         //Metod som returnerar Booms
